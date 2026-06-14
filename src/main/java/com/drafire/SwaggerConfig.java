f37c40cabf001c6d254040a426eff5b8a2e7a520
drafire <hyljljlj@163.com>
2026-06-14 18:40:00 +0800

    first commit

---
+++ src/main/java/com/drafire/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.drafire;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring AI Alibaba Helloworld API")
+                        .version("1.0.0")
+                        .description("Spring AI Alibaba Helloworld Example - AI 聊天助手 API 文档")
+                        .contact(new Contact()
+                                .name("yuluo")
+                                .email("yuluo08290126@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
